8d0043d82b59c8fc0456ab4574945beb276136e0
Anna Lawrence <44843977+alawrence1985@users.noreply.github.com>
2021-10-11 13:25:24 +0000

    Added new sprites for game and created pumpkin movement/scoring system.

--- trexJump.py
+++ trexJump.py
@@ -1,11 +1,28 @@
 # Tutorial: https://pygame-zero.readthedocs.io/en/stable/introduction.html
 
-alien = Actor('alien')
-alien.pos = 100, 56
+WIDTH = 400
+HEIGHT = 250
 
-WIDTH = 500
-HEIGHT = alien.height + 20
+trex = Actor('trex')
+trex.pos = trex.width*2, HEIGHT-trex.height//2
+pumpkin = Actor('pumpkin')
+pumpkin.pos = WIDTH-pumpkin.width//2, HEIGHT-pumpkin.height//2
+
+score = 0
 
 def draw():
     screen.clear()
-    alien.draw()
+    screen.fill("black")
+    trex.draw()
+    pumpkin.draw()
+
+def update():
+  move_pumpkin()
+
+def move_pumpkin():
+  global score
+  pumpkin.x -= 2
+
+  if pumpkin.x <= 0:
+    score += 1
+    pumpkin.x = WIDTH-pumpkin.width//2
