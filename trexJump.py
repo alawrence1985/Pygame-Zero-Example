4ea39f451aa6361e604e43408fe4806c73971aa4
Anna Lawrence <44843977+alawrence1985@users.noreply.github.com>
2021-10-12 12:51:59 +0000

    Added trex jump equation and collision detection for game over scenario

--- trexJump.py
+++ trexJump.py
@@ -1,4 +1,4 @@
-# Tutorial: https://pygame-zero.readthedocs.io/en/stable/introduction.html
+import math
 
 WIDTH = 400
 HEIGHT = 250
@@ -9,20 +9,49 @@ pumpkin = Actor('pumpkin')
 pumpkin.pos = WIDTH-pumpkin.width//2, HEIGHT-pumpkin.height//2
 
 score = 0
+GROUND = HEIGHT-trex.height//2
+RADIAN = math.pi/64
+nextRadian = 1
+isJumping = False
+game_over = False
+
 
 def draw():
     screen.clear()
     screen.fill("black")
-    trex.draw()
-    pumpkin.draw()
+    if not game_over:
+      trex.draw()
+      pumpkin.draw()
+      screen.draw.text("SCORE: " + str(score), (5, 10), color="orange")
+    else:
+      screen.draw.text("GAME OVER :(((((", (125, 100), color="orange")
 
 def update():
-  move_pumpkin()
+  update_trex()
+  update_pumpkin()
 
-def move_pumpkin():
-  global score
+def update_trex():
+  global nextRadian, isJumping
+
+  if keyboard.up and not isJumping:
+    isJumping = True
+    trex.y = -90*math.sin(nextRadian*RADIAN) + GROUND
+    nextRadian = nextRadian + 1
+  elif trex.y < GROUND and isJumping:
+    trex.y = -90*math.sin(nextRadian*RADIAN) + GROUND
+    nextRadian = nextRadian + 1
+  else:
+    isJumping = False
+    nextRadian = 1
+
+def update_pumpkin():
+  global score, game_over
+
   pumpkin.x -= 2
 
   if pumpkin.x <= 0:
     score += 1
     pumpkin.x = WIDTH-pumpkin.width//2
+  
+  if pumpkin.colliderect(trex):
+    game_over = True
